dd44b27197187c15396f9c22dbfa1eaea938e59d
Kar111 <avetisyankarine111@gmail.com>
2023-01-27 02:29:37 +0400

    Copy the array in an ansync way, replace "a" with "%".

---
+++ src/AsyncReplacement.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class AsyncReplacement {
+
+
+    public String[] asyncArrayCopyWithReplacement(String[] source) {
+        String[] destination = new String[source.length];
+
+        int treadNumber = 11;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(treadNumber);
+
+        for(int i = 0; i < source.length; i++) {
+            final int index = i;
+            executorService.execute( () -> {
+                destination[index] = source[index].replace("a", "%");
+                    }
+            );
+        }
+
+        executorService.shutdown();
+
+        while(!executorService.isTerminated()) {
+        }
+
+        return destination;
+    }
+}
